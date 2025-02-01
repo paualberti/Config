4cc55ea3be0e8b8b196e2629425a20312a6e5900
paualberti <paualberti73@gmail.com>
2025-02-01 03:46:38 +0100

    lsp

--- nvim/lua/paualberti/plugins/conform.lua
+++ nvim/lua/paualberti/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
 		cmd = { "ConformInfo" },
 		keys = {
 			{
-				"<leader>bf",
+				"<leader>lf",
 				function()
 					require("conform").format({ async = true, lsp_format = "fallback" })
 				end,
--- nvim/lua/paualberti/plugins/lspconfig.lua
+++ nvim/lua/paualberti/plugins/lspconfig.lua
@@ -64,9 +64,8 @@ return {
 					--
 					-- In this case, we create a function that lets us more easily define mappings specific
 					-- for LSP related items. It sets the mode, buffer and description for us each time.
-					local map = function(keys, func, desc, mode)
-						mode = mode or "n"
-						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					local vim_keymap_set = function(mode, keys, func, desc)
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
 					end
 
 					-- Jump to the definition of the word under your cursor.
@@ -95,53 +94,53 @@ return {
 					})
 
 					local builtin = require("telescope.builtin")
-					map("<leader>lgd", function()
+					vim_keymap_set("n", "gd", function()
 						builtin.lsp_definitions(theme)
 					end, "Definition")
 
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
-					map("<leader>lgD", function()
+					vim_keymap_set("n", "gD", function()
 						vim.lsp.buf.declaration(theme)
 					end, "Declaration")
 
 					-- Find references for the word under your cursor.
-					map("<leader>lgr", function()
+					vim_keymap_set("n", "gR", function()
 						builtin.lsp_references(theme)
 					end, "References")
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("<leader>lgI", function()
+					vim_keymap_set("n", "gI", function()
 						builtin.lsp_implementations(theme)
 					end, "Implementation")
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map("<leader>ld", function()
+					vim_keymap_set("n", "<leader>ld", function()
 						builtin.lsp_type_definitions(theme)
 					end, "Definition")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>lsd", function()
+					vim_keymap_set("n", "<leader>ld", function()
 						builtin.lsp_document_symbols(theme)
-					end, "Document")
+					end, "Document Symbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
-					map("<leader>lsw", function()
+					vim_keymap_set("n", "<leader>lw", function()
 						builtin.lsp_dynamic_workspace_symbols(theme)
-					end, "Workspace")
+					end, "Workspace Symbols")
 
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
-					map("<leader>lr", vim.lsp.buf.rename, "Rename")
+					vim_keymap_set("n", "<leader>lr", vim.lsp.buf.rename, "Rename")
 
 					-- Execute a code action, usually your cursor needs to be on top of an error
 					-- or a suggestion from your LSP for this to activate.
-					map("<leader>lc", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
+					vim_keymap_set({ "n", "x" }, "<leader>lc", vim.lsp.buf.code_action, "Code Action")
 				end,
 			})
 
